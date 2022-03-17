bf7099c9764f48d1c91d0fe2bc6970d0cf05d409
Tom Kim <tom.kim@3i.ai>
2022-03-18 02:48:54 +0900

    - added network module - added glide gradle

--- app/src/main/java/com/jobplanet/company/di/NetworkModule.kt
+++ app/src/main/java/com/jobplanet/company/di/NetworkModule.kt
@@ -1,4 +1,49 @@
 package com.jobplanet.company.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient() : OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = Level.BODY
+
+        return OkHttpClient.Builder()
+            .readTimeout(10, TimeUnit.SECONDS)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient, baseUrlV2: String, gsonConverterFactory: GsonConverterFactory) : Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(baseUrlV2)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
 }
---
+++ app/src/main/java/com/jobplanet/company/di/RepositoryModule.kt
@@ -0,0 +1,12 @@
+package com.jobplanet.company.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+interface RepositoryModule {
+
+}
