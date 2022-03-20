9a4f647a2248661ed6aff2d608e4538b10d0cbc1
Tom Kim <tom.kim@3i.ai>
2022-03-21 04:00:24 +0900

    - fixed theme for dark mode

--- app/src/main/java/com/jobplanet/company/ui/company/company/CompanyViewModel.kt
+++ app/src/main/java/com/jobplanet/company/ui/company/company/CompanyViewModel.kt
@@ -1,17 +1,21 @@
 package com.jobplanet.company.ui.company.company
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CompanyViewModel @Inject constructor(): ViewModel(){
 
-    val moreCompanyInfoState = MutableStateFlow<String>("")
+    val moreCompanyInfoState = MutableSharedFlow<String>()
 
     fun onMoreCompanyInfoClick(companyName:String) {
-        moreCompanyInfoState.value = ""
-        moreCompanyInfoState.value = companyName
+        viewModelScope.launch {
+            moreCompanyInfoState.emit(companyName)
+        }
     }
 }
--- app/src/main/java/com/jobplanet/company/ui/company/review/ReviewViewModel.kt
+++ app/src/main/java/com/jobplanet/company/ui/company/review/ReviewViewModel.kt
@@ -1,8 +1,11 @@
 package com.jobplanet.company.ui.company.review
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,10 +13,11 @@ class ReviewViewModel @Inject constructor(
 
 ): ViewModel(){
 
-    val moreReviewInfoState = MutableStateFlow<String>("")
+    val moreReviewInfoState = MutableSharedFlow<String>()
 
     fun onMoreReviewInfoClick(companyName:String) {
-        moreReviewInfoState.value = ""
-        moreReviewInfoState.value = companyName
+        viewModelScope.launch {
+            moreReviewInfoState.emit(companyName)
+        }
     }
 }
