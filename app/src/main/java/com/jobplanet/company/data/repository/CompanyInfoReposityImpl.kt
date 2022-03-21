37e04fafa8ddb147a5957c8e9707a809ce81dda9
tom.kim <tom.kim@3i.ai>
2022-03-22 08:36:32 +0900

    - added custom annotation for dispatcher as google provided. see https://developer.android.com/kotlin/coroutines/coroutines-best-practices

--- app/src/main/java/com/jobplanet/company/data/repository/CompanyInfoReposityImpl.kt
+++ app/src/main/java/com/jobplanet/company/data/repository/CompanyInfoReposityImpl.kt
@@ -1,21 +1,22 @@
 package com.jobplanet.company.data.repository
 
 import com.jobplanet.company.data.source.remote.service.CompanySearchService
+import com.jobplanet.company.di.AppModule
 import com.jobplanet.company.util.Resource
 import com.jobplanet.company.domain.model.*
 import com.jobplanet.company.domain.repository.CompanyRepository
 import kotlinx.coroutines.*
 import javax.inject.Inject
-import kotlin.coroutines.resume
 
 class CompanyInfoRepositoryImpl @Inject constructor(
-    private val companySearchService: CompanySearchService
+    private val companySearchService: CompanySearchService,
+    @AppModule.IODispatcher private val defaultDispatcher: CoroutineDispatcher
 ): CompanyRepository {
 
     /**
      * 서버로부터 기업정보 리스트 받아옴
      */
-    override suspend fun getCompanyList() = withContext(Dispatchers.IO) {
+    override suspend fun getCompanyList() = withContext(defaultDispatcher) {
         val response = companySearchService.getCompanyList()
         val result = if(response.isSuccessful) {
             val list = response.body()
